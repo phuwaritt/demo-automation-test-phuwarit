ae9a904bdbd5183a0f543b1602092a7baaa45938
VittavasTan <vittavas.tan@gmail.com>
2025-02-25 20:44:08 +0700

    updates

--- common.groovy
+++ common.groovy
@@ -1,8 +1,11 @@
-def testFun(){
-    echo "starting simulator..."
-}
+APPIUM_PORT= [4723, 4724]
 
-return this
+def notify_line(passed_count, failed_count, log_url, BRANCH_NAME){
+    def token = "lWcrOHVVskzMYRXb7iB1e9xanhcvmC3Pu8Jz39Ozufh"
+    def url = 'https://notify-api.line.me/api/notify'
+    def message = "BPY 🔥 \nBranch: ${BRANCH_NAME} Build ${env.BUILD_NUMBER} \nPassed : ${passed_count} ✅ \nFailed : ${failed_count} 🤮 \nAfter:${currentBuild.durationString} \n\n(${log_url})"
+    sh "curl ${url} -H 'Authorization: Bearer ${token}' -F 'message=${message}'"
+}
 
 def report_sender(){
     echo "=== Start notify to failure declaration ==="
@@ -10,6 +13,18 @@ def report_sender(){
     ls -la
     python3 ./report_sender.py
     '''
-    // }
     echo "report_sender done"
 }
+
+def stop_appium(){
+    for(int i in APPIUM_PORT){
+        echo "STOP Appium Port ${i}"
+        try {
+            sh "kill -9 \$(lsof -t -i :${i})"
+        }
+        catch(err) {
+            echo "appium ${i} is stopped"
+        }
+    }
+}
+
