e54d5bca3b65153dd0e20f275e10b25630be550f
VittavasTan <vittavas.tan@gmail.com>
2025-02-25 20:51:05 +0700

    Update common.groovy

--- common.groovy
+++ common.groovy
@@ -1,11 +1,13 @@
 APPIUM_PORT= [4723, 4724]
 
+
 def notify_line(passed_count, failed_count, log_url, BRANCH_NAME){
     def token = "lWcrOHVVskzMYRXb7iB1e9xanhcvmC3Pu8Jz39Ozufh"
     def url = 'https://notify-api.line.me/api/notify'
     def message = "BPY 🔥 \nBranch: ${BRANCH_NAME} Build ${env.BUILD_NUMBER} \nPassed : ${passed_count} ✅ \nFailed : ${failed_count} 🤮 \nAfter:${currentBuild.durationString} \n\n(${log_url})"
     sh "curl ${url} -H 'Authorization: Bearer ${token}' -F 'message=${message}'"
 }
+
 
 def report_sender(){
     echo "=== Start notify to failure declaration ==="
@@ -15,6 +17,7 @@ def report_sender(){
     '''
     echo "report_sender done"
 }
+
 
 def stop_appium(){
     for(int i in APPIUM_PORT){
