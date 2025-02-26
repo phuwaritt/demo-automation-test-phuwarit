de40dd85e252c49989e5527ee870cc8e10d66c3d
VittavasTan <vittavas.tan@gmail.com>
2025-02-26 09:14:09 +0700

    move emoji_function to common.groovy

--- common.groovy
+++ common.groovy
@@ -1,6 +1,17 @@
 APPIUM_PORT= [4723, 4724]
 
 
+def result_jenkins_emoji(failed_count){
+    def message_emoji = ""
+    if (failed_count.toInteger() == 0) {
+        message_emoji = ":671ce8d3a35105bd5feefb5e006e749f:"
+    } else {
+        message_emoji = ":yuno:"
+    }
+    return message_emoji
+}
+
+
 def notify_line(passed_count, failed_count, log_url, BRANCH_NAME){
     def token = "lWcrOHVVskzMYRXb7iB1e9xanhcvmC3Pu8Jz39Ozufh"
     def url = 'https://notify-api.line.me/api/notify'
