03aa472c731f1997f0f314e9ac111f6ec86f5e40
VittavasTan <vittavas.tan@gmail.com>
2025-02-28 16:24:00 +0700

    Update common.groovy

--- common.groovy
+++ common.groovy
@@ -68,7 +68,7 @@ def start_appium() {
     for(int i=0; i<APPIUM_PORT.size(); i++){
         echo "STARTING Appium Port ${APPIUM_PORT[i]}"
         try {
-            sh "appium -p ${APPIUM_PORT[i]} &"
+            sh "appium -p ${APPIUM_PORT[i]} -allow-cors --driver-xcuitest-webdriveragent-port ${WDA_PORT[i]} &"
         }
         catch(err) {
             echo "appium ${APPIUM_PORT[i]} is started"
